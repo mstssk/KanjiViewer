6cef070a0255fc9802abd78b5c33dadd897e42e9
mstssk <mstssk+github@gmail.com>
2013-05-01 02:01:51 +0900

    設定がDefaultSharedPreferenceを使っていなかったので修正

--- src/jp/mstssk/kanji_viewer/KanjiViewerPrefs.java
+++ src/jp/mstssk/kanji_viewer/KanjiViewerPrefs.java
@@ -5,13 +5,14 @@ import android.graphics.Color;
 
 import com.googlecode.androidannotations.annotations.sharedpreferences.DefaultInt;
 import com.googlecode.androidannotations.annotations.sharedpreferences.SharedPref;
+import com.googlecode.androidannotations.annotations.sharedpreferences.SharedPref.Scope;
 
 /**
  * 漢字ビューワの設定項目
  * 
  * @author mstssk
  */
-@SharedPref
+@SharedPref(Scope.APPLICATION_DEFAULT)
 public interface KanjiViewerPrefs {
 
     // XXX 各項目の名前が残念な感じだが、名とpreferecneのkeyは一致させなければいけないので
